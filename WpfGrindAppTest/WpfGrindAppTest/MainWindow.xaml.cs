63968a21ebecd87407a8faacb133b38ea5bb0b02
morbezabal <morbezabal@avangroup.com>
2018-04-17 08:45:14 +0200

    20180417 Primera carga del proyecto

---
+++ WpfGrindAppTest/WpfGrindAppTest/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfGrindAppTest
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResultadoText_Click(object sender, RoutedEventArgs e)
+        {
+            tbResultadoText.Text = string.Empty;
+            if (txtTexto.Text!=String.Empty)
+            {
+                tbResultadoText.Foreground = Brushes.Black;
+                tbResultadoText.Text = txtTexto.Text;
+            }
+            else
+            {
+                tbResultadoText.Foreground = Brushes.Red;
+                tbResultadoText.Text = "You must enter any text";
+            }
+        }
+    }
+}
